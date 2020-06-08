863138cb5d2c8ee095391d7653fd6dc70077ae45
Chuck Konkol <ckonkol@gmail.com>
2020-06-08 13:08:18 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -32,9 +32,27 @@ function checkCookie() {
       return true;
     } else {
       return false;    
-      var date = new Date();
-      date.setTime(date.getTime() + (30 * 1000));
-      setCookie("notification","notif", date);
+      setCookie("notification","notif", 2);
     }
 }
-
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime()+(hours*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
