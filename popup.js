ee9a26c725e723d022214f3d24ee3477207fb294
Chuck Konkol <ckonkol@gmail.com>
2020-06-08 12:02:28 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 jQuery.get('notification.txt', function(data) {
     alert(data);
-        var createDialog = function(data , "1-Minute Guide") {
+        var createDialog = function(data , '1-Minute Guide') {
          var dialog =  "<div id=dialog <h1>" + data + "</h1></div>";
         $('body').append(dialog);
-        $('#dialog').prop('title' , title);
+        $('#dialog').prop('title' , '1-Minute Guide');
          $('#dialog').dialog();
          }
 });
