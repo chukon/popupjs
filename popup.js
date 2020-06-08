ec51af54de9fa9c2ada70c9208e1403d2ebfd6cd
Chuck Konkol <ckonkol@gmail.com>
2020-06-08 11:22:13 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
-window.onload = function(){
-var fs = require('fs');
- 
+var fs = require('fs'); 
 fs.readFile('notification.txt', 'utf8', function(err, contents) {
     console.log(contents);
-});
- 
+}); 
 console.log('after calling readFile');
-};
 
