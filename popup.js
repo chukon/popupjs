995b4160eb173add729c578f37edd6cd5c4d02f4
Chuck Konkol <ckonkol@gmail.com>
2020-06-08 10:28:43 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,4 @@
-window.onload = function() {
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  //Open a file on the server and return its content:
-  fs.readFile('notification.txt', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(80);
+window.onload = function(){
+let content = fs.readFileSync('notification.txt', 'utf8');
+alert(content);
 };
